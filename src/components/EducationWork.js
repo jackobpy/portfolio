ed907e998fb9b069a7d3f632a70caa19f400298f
Jakub Fręchowicz <j.s.frechowicz@student.tudelft.nl>
2024-11-19 22:33:51 +0100

    Update work and projects

--- src/components/EducationWork.js
+++ src/components/EducationWork.js
@@ -6,12 +6,16 @@ import workschool from '../assets/contents/workschool.json';
 import tudelft from '../assets/img/tudelft.png';
 import staszic from '../assets/img/staszic.png';
 import lanterna from '../assets/img/lanterna.png';
+import prime from '../assets/img/prime.jpg';
+import teachbooks from '../assets/img/teachbooks.png';
 
 export const EducationWork = () => {
     const uni = workschool[0]
     const hs = workschool[1]
     const ta = workschool[2]
     const tut = workschool[3]
+    const prm = workschool[4]
+    const tb = workschool[5]
     return(
         <div className="education-work">
             <section id="education">
@@ -37,6 +41,18 @@ export const EducationWork = () => {
                 </Row>
                 <Row>
                     <SchoolWork dates={ta.dates} title={ta.title} subtitle={ta.subtitle} feature={ta.feature} paragraph={ta.paragraph} imagepath={tudelft} imagelink={ta.imagelink} leftAligned={ta.leftAligned}/>
+                </Row>
+                <Row className="d-flex justify-content-center">
+                    <Image src={stroke2} style={{width: '500px'}} className='stroke'/>
+                </Row>
+                <Row className="d-flex justify-content-end">
+                    <SchoolWork dates={prm.dates} title={prm.title} subtitle={prm.subtitle} feature={prm.feature} paragraph={prm.paragraph} imagepath={prime} imagelink={prm.imagelink} leftAligned={prm.leftAligned}/>
+                </Row>
+                <Row className="d-flex justify-content-center">
+                    <Image src={stroke1} style={{width: '500px'}} className='stroke flipped'/>
+                </Row>
+                <Row>
+                    <SchoolWork dates={tb.dates} title={tb.title} subtitle={tb.subtitle} feature={tb.feature} paragraph={tb.paragraph} imagepath={teachbooks} imagelink={tb.imagelink} leftAligned={tb.leftAligned}/>
                 </Row>
                 <Row className="d-flex justify-content-center">
                     <Image src={stroke2} style={{width: '500px'}} className='stroke'/>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import img2 from '../assets/img/website.png'
 import img from '../assets/img/diff.png'
-import img3 from '../assets/img/numja.png'
+import img3 from '../assets/img/sphinxtudelfttheme.png'
 import img4 from '../assets/img/splitty.png'
 
 export const Projects = () => {
@@ -37,20 +37,28 @@ export const Projects = () => {
                     </Col>
                     <Col style={{marginTop: '0px'}}>
                         <Card style={{marginBottom: '20px'}}>
+                            <Card.Img variant="top" src={img3} />
+                            <Card.Body>
+                            <Card.Title style={{fontSize: '30px', fontWeight: '500'}}>Sphinx TU Delft theme - Python package</Card.Title>
+                            <Card.Text style={{fontSize: '18px', fontWeight: '500'}}>
+                            The Sphinx-TUDelft-theme extension provides a simple solution to apply a uniform theme across all the books created with TeachBooks at Delft University of Technology that matches the TU Delft identity. Here you can find its&nbsp;
+                            <a href={"https://github.com/TeachBooks/Sphinx-TUDelft-theme"} target="_blank" rel="noopener noreferrer">
+                                GitHub repository
+                            </a>
+                            &nbsp;and&nbsp;
+                            <a href={"https://pypi.org/project/sphinx-tudelft-theme/0.0.6/#description"} target="_blank" rel="noopener noreferrer">
+                                 PyPI page
+                            </a>
+                            .
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card style={{marginBottom: '20px'}}>
                             <Card.Img variant="top" src={img} />
                             <Card.Body>
                             <Card.Title style={{fontSize: '30px', fontWeight: '500'}}>diff implementation in assembly</Card.Title>
                             <Card.Text style={{fontSize: '18px', fontWeight: '500'}}>
                             Together with another student we built an Assembly x86-64 version of ‘diff’ linux command, which compares two files line by line and shows all the differences between them.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card style={{marginBottom: '20px'}}>
-                            <Card.Img variant="top" src={img3} />
-                            <Card.Body>
-                            <Card.Title style={{fontSize: '30px', fontWeight: '500'}}>numJa - numPy in Java</Card.Title>
-                            <Card.Text style={{fontSize: '18px', fontWeight: '500'}}>
-                                With a group of friends we are developing a Java library for performing operations on large, multi-dimensional arrays and matrices.
                             </Card.Text>
                             </Card.Body>
                         </Card>
